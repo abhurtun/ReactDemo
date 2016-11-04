3f57d7e03b978f92d81f686d2ad6f8fa8bfc0ee9
Arvin Bhurtun <abhurtun@dctfh.com>
2016-11-04 10:03:17 +0000

    Tutorial using react and asp.net core https://reactjs.net/getting-started/aspnetcore.html

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build Status](https://travis-ci.org/abhurtun/ReactDemo.svg?branch=master)](https://travis-ci.org/abhurtun/ReactDemo)
+# ReactDemoNETCore
---
+++ src/ReactDemo/wwwroot/js/tutorial.jsx
@@ -0,0 +1,36 @@
+﻿var CommentBox = React.createClass({displayName: 'CommentBox',
+  render: function() {
+      return (
+        <div className="commentBox">
+          <h1>Comments tat</h1>
+          <CommentList />
+          <CommentForm />
+        </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        return (
+          <div className="commentList">
+            Hello, world! I am a CommentList.
+          </div>
+      );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+          <div className="commentForm">
+            Hello, world! I am a CommentForm.
+          </div>
+      );
+    }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
